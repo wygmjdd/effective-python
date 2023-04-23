41185425200c53a7a259eaacd7cbfa2901a7a036
我要改名叫嘟嘟 <ljj0954@gmail.com>
2023-04-23 18:58:21 +0800

    feat: 生成器的最末.

--- chapter4/36_use_itertools.py
+++ chapter4/36_use_itertools.py
@@ -61,3 +61,48 @@ values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 less_than_seven = lambda x: x < 7  # NOQA
 it = itertools.dropwhile(less_than_seven, values)
 print('dropwhile: ', list(it))
+
+
+# 9. 筛选函数
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = lambda x: x % 2 == 0  # NOQA
+filter_result = filter(evens, values)
+print('filter: ', list(filter_result))
+filter_false_result = itertools.filterfalse(evens, values)
+print('filter false:', list(filter_false_result))
+
+# 10. 累加值
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum_reduce = itertools.accumulate(values)
+print('accumulate:', list(sum_reduce))
+
+
+def sum_modulo_20(first, second):
+    output = first + second
+    return output % 20
+
+
+modulo_reduce = itertools.accumulate(values, sum_modulo_20)
+print('accumulate modulo:', list(modulo_reduce))
+
+
+# 11. 计算笛卡尔积
+single = itertools.product([1, 2, 3], repeat=2)
+print('product single:', list(single))
+multiple = itertools.product([1, 2], ['a', 'b', 'c'])
+print('product multiple:', list(multiple))
+
+
+# 12. 排列，并且是有序的
+it = itertools.permutations([1, 2, 3, 4], 2)
+print('permutations:', list(it))
+
+
+# 13. 组合，也是有序的
+it = itertools.combinations([1, 2, 3, 4], 2)
+print('combinations:', list(it))
+
+
+# 14. 多次出现的组合
+it = itertools.combinations_with_replacement([1, 2, 3, 4], 2)
+print('combinations_with_replacement:', list(it))
